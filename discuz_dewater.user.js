3453f8ce7000c82697b02e0ee51a291587084e50
fourth0100 <36062032+fourth0100@users.noreply.github.com>
2019-05-14 16:56:34 +0800

    Update discuz_dewater.user.js

--- discuz_dewater.user.js
+++ discuz_dewater.user.js
@@ -2,7 +2,7 @@
 //
 // ==UserScript==
 // @name          forum_dewater 
-// @version       0.3.3
+// @version       0.3.4
 // @description   forum 论坛贴子脱水
 // @copyright     fork by ：2013, Abby Pan (http://abbypan.github.com/) 
 // @grant         GM_getResourceText
